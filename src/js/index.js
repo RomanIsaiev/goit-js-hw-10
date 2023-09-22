e61b7fc0080a51d14c7705a30e66bf6b12e4bd15
cloudwipe <roma.isaev.2000@gmail.com>
2023-09-22 22:53:32 +0300

    working

--- src/js/index.js
+++ src/js/index.js
@@ -20,10 +20,23 @@ const api_key =
   'live_ds7IOLf26S1PWwqgPFrJeWA3DKWOVsAilq9eQpOhrh5ZJrNWtWQLnqoUn19c2yKi';
 
 fetchBreeds();
+// .then(data => {
+//   renderBreedsCollection(data);
+//   const breedId = refs.breedSelector.value;
+//   console.log(breedId);
+//   fetchCatByBreed(breedId)
+//     .then(renderCatCard)
+//     .catch(error => {
+//       refs.error.style.display = 'block';
+//       console.log(error);
+//     });
+// })
+// .catch(error => {
+//   refs.error.style.display = 'block';
+//   console.log(error);
+// });
 
 function fetchBreeds() {
-  let storedBreeds = [];
-
   return fetch(url, {
     headers: {
       'x-api-key': api_key,
@@ -35,22 +48,16 @@ function fetchBreeds() {
       return response.json();
     })
     .then(data => {
-      refs.breedSelector.style.display = 'block';
-      refs.loader.style.display = 'none';
-
-      storedBreeds = data;
-      for (let i = 0; i < storedBreeds.length; i++) {
-        const breed = storedBreeds[i];
-        let option = document.createElement('option');
-        option.value = `${breed.id}`;
-        option.innerHTML = `${breed.name}`;
-
-        document.querySelector('.breed-select').appendChild(option);
-      }
+      renderBreedsCollection(data);
       const breedId = refs.breedSelector.value;
-      fetchCatByBreed(breedId);
+      fetchCatByBreed(breedId)
+        .then(renderCatCard)
+        .catch(error => {
+          refs.error.style.display = 'block';
+          console.log(error);
+        });
     })
-    .catch(function (error) {
+    .catch(error => {
       refs.error.style.display = 'block';
       console.log(error);
     });
@@ -64,22 +71,23 @@ function fetchCatByBreed(breedId) {
         'x-api-key': api_key,
       },
     }
-  )
-    .then(response => {
-      refs.catContainer.style.display = 'none';
-      refs.loader.style.display = 'block';
-      return response.json();
-    })
-    .then(data => {
-      refs.loader.style.display = 'none';
-      refs.catContainer.style.display = 'block';
-      const breeds = data[0].breeds;
-      const catName = breeds[0].name;
-      const catDesc = breeds[0].description;
-      const catTemp = breeds[0].temperament;
-      const catImage = data[0].url;
+  ).then(response => {
+    refs.catContainer.style.display = 'none';
+    refs.loader.style.display = 'block';
+    return response.json();
+  });
+}
 
-      const markup = `<div class="cat-container">
+function renderCatCard(data) {
+  refs.loader.style.display = 'none';
+  refs.catContainer.style.display = 'block';
+  const breeds = data[0].breeds;
+  const catName = breeds[0].name;
+  const catDesc = breeds[0].description;
+  const catTemp = breeds[0].temperament;
+  const catImage = data[0].url;
+
+  const markup = `<div class="cat-container">
         <img class="cat-image" src="${catImage}" width="450" height="350" />
         <div>
         <h2 class="cat-name">${catName}</h2>
@@ -88,10 +96,21 @@ function fetchCatByBreed(breedId) {
         </div>
       </div>`;
 
-      refs.catContainer.innerHTML = markup;
-    })
-    .catch(error => {
-      refs.error.style.display = 'block';
-      console.log(error);
-    });
+  refs.catContainer.innerHTML = markup;
+}
+
+function renderBreedsCollection(data) {
+  let storedBreeds = [];
+  refs.breedSelector.style.display = 'block';
+  refs.loader.style.display = 'none';
+
+  storedBreeds = data;
+  for (let i = 0; i < storedBreeds.length; i++) {
+    const breed = storedBreeds[i];
+    let option = document.createElement('option');
+    option.value = `${breed.id}`;
+    option.innerHTML = `${breed.name}`;
+
+    document.querySelector('.breed-select').appendChild(option);
+  }
 }
